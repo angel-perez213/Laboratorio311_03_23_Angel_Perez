20eaed5c6ce6194fa33421ec441523fc2f248ea5
perez <perez@LAPTOP-RDFT1A1N>
2023-03-11 09:54:16 -0800

    Add project files.

---
+++ Laboratorio11_03_23_Angel_Perez/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Laboratorio11_03_23_Angel_Perez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a. Sumatoria");
+            Console.WriteLine("b. Tablas de multiplicar");
+            char opcion = Convert.ToChar(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 'a':
+
+                    Console.WriteLine("Ingrese un numero:");
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    int resultado = 0;
+
+
+                    do
+                    {
+                        resultado = resultado + n;
+                        n--;
+                    } while (n > 0);
+
+                    Console.WriteLine("Sumatoria" + resultado);
+                    break;
+
+                case 'b':
+                    for (int i = 1; i < 11; i++)
+                    {
+                        Console.Write(i + ": ");
+                        for (int j = i; j < 11; j++)
+                        {
+                            Console.Write((i * j) + "\t");
+                        }
+                        Console.Write("\n");
+                    }
+                    break;
+            }
+            
+
+           
+        }
+    }
+}
